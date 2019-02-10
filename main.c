63f023f06b702c759686c539170cea6041ad1c83
iyesildirek <26639277+iyesildirek@users.noreply.github.com>
2019-02-10 13:42:57 -0600

    Updated main.c header

---
+++ main.c
@@ -0,0 +1,31 @@
+/*****************************************************************************
+* Copyright (C) 2019 by Ismail Yesildirek & Bijan Kianian
+*
+* Redistribution, modification or use of this software in source or binary
+* forms is permitted as long as the files maintain this copyright. Users are
+* permitted to modify this and use it to learn about the field of embedded
+* software. Ismail Yesildirek, Bijan Kianian, and the University of Colorado are not 
+* liable for any misuse of this material.
+*
+*****************************************************************************/
+/**
+* @file Main.C
+* @brief This source file contains a c program to xxxx. 
+*
+* @author Ismail Yesildirek & Bijan Kianian
+* @date February X 2019
+* @version 1.0
+*
+*/
+
+#include<stdio.h>
+#include<stdint.h> 
+
+/**
+* @brief 
+*/
+
+void main ()
+{
+	
+}
