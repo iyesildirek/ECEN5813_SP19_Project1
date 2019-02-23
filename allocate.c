e23760cca5515a24029e092939383e22604a7448
iyesildirek <26639277+iyesildirek@users.noreply.github.com>
2019-02-23 10:32:21 -0600

    Updated file comment header

--- allocate.c
+++ allocate.c
@@ -1,7 +1,24 @@
+/*****************************************************************************
+* Copyright (C) 2019 by Ismail Yesildirek & Bijan Kianian
+*
+* Redistribution, modification or use of this software in source or binary
+* forms is permitted as long as the files maintain this copyright. Users are
+* permitted to modify this and use it to learn about the field of embedded
+* software. Ismail Yesildirek, Bijan Kianian, and the University of Colorado are not
+* liable for any misuse of this material.
+*
+*****************************************************************************/
+/**
+* @file allocate.c
+* @brief This source file contains code that provides the allocate() function definition 
+* and frees the space allocated in the memory pointed by ptr.
+*
+* @author Ismail Yesildirek & Bijan Kianian
+* @date February 23 2019
+* @version 1.0
+*
+*/
 
-/*===================================*/
-/*            allocate()             */
-/*===================================*/
 #include "memtest.h"
 
 int* allocate(int offset)
--- free.c
+++ free.c
@@ -1,6 +1,23 @@
-/*===================================*/
-/*          free_memory()           */
-/*===================================*/
+/*****************************************************************************
+* Copyright (C) 2019 by Ismail Yesildirek & Bijan Kianian
+*
+* Redistribution, modification or use of this software in source or binary
+* forms is permitted as long as the files maintain this copyright. Users are
+* permitted to modify this and use it to learn about the field of embedded
+* software. Ismail Yesildirek, Bijan Kianian, and the University of Colorado are not
+* liable for any misuse of this material.
+*
+*****************************************************************************/
+/**
+* @file free.c
+* @brief This source file contains code that provides the free() function definition 
+* and reserves a block of memory of the specified number of bytes.
+*
+* @author Ismail Yesildirek & Bijan Kianian
+* @date February 23 2019
+* @version 1.0
+*
+*/
 
 #include "memtest.h"
 
